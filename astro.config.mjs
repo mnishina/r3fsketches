7859d96274fbadd0666c249cc797b535c632cb37
M Nishina <mitsunorinishina@icloud.com>
2024-10-26 13:23:34 +0900

    glslを使えるようにした。

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from "@astrojs/vercel/serverless";
+
+import glsl from "vite-plugin-glsl";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  output: 'server',
-  adapter: vercel()
+  output: "server",
+  adapter: vercel(),
+  vite: {
+    plugins: [glsl()],
+  },
 });
