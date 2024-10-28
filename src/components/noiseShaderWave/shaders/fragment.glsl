63025e0dea187b6dce54c20ca81fb7bcc474a40e
M Nishina <mitsunorinishina@icloud.com>
2024-10-28 16:25:32 +0900

    波の影響を色にも反映

--- src/components/noiseShaderWave/shaders/fragment.glsl
+++ src/components/noiseShaderWave/shaders/fragment.glsl
@@ -2,12 +2,14 @@ uniform float uTime;
 uniform sampler2D uImageTex;
 
 varying vec2 vUv;
+varying float vWave;
 
 void main() {
   
-  vec4 imageTex = texture(uImageTex, vUv);
+  float wave = vWave * 0.05; // ぐちゃぐちゃになるので数値をかけてる
+  vec3 imageTex = texture(uImageTex, vUv + wave).rgb;
 
-  gl_FragColor = imageTex;
+  gl_FragColor = vec4(imageTex, 1.0);
 
   // #include <tonemapping_fragment>
   // #include <colorspace_fragment>
