5e31ac8f28f8f820ba46daae7a114611cdc7e3e2
M Nishina <mitsunorinishina@icloud.com>
2024-11-02 21:16:24 +0900

    Created shaders

---
+++ src/components/shaderLighting/shaders/fragmentShader.glsl
@@ -0,0 +1,33 @@
+varying vec2 vUv;
+varying vec3 vNormal;
+varying vec3 vPosition;
+
+uniform vec3 uColor;
+
+#include ./includes/ambientLight.glsl
+#include ./includes/directionalLight.glsl
+
+void main() {
+  vec3 normal = normalize(vNormal);
+  vec3 viewDirection = normalize(vPosition - cameraPosition);
+  vec3 color = uColor;
+
+  //Light
+  vec3 light = vec3(0.0);
+  light += ambientLight(vec3(1.0), 0.03);
+  light += directionalLight(
+    vec3(0.1,0.1,1.0),
+    2.0,
+    normal,
+    vec3(0,0,3.0),
+    viewDirection,
+    20.0
+  );
+
+  color *= light;
+
+  gl_FragColor = vec4(color,1.0);
+
+  #include <tonemapping_fragment>
+  #include <colorspace_fragment>
+}
