967257c21118d720b3282ec95e385484e1cc4aab
M Nishina <mitsunorinishina@icloud.com>
2024-11-04 17:53:50 +0900

    Create particles

--- src/components/particlesBasic/world.ts
+++ src/components/particlesBasic/world.ts
@@ -18,6 +18,10 @@ interface World {
   aspectRatio: number;
   near: number;
   far: number;
+  loadManager: THREE.LoadingManager;
+  textureLoader: THREE.TextureLoader;
+  particleTex: THREE.Texture | null;
+  texPath: string;
 }
 
 const world: World = {
@@ -36,18 +40,24 @@ const world: World = {
   aspectRatio: 0,
   near: 0.1,
   far: 1000,
+  loadManager: new THREE.LoadingManager(),
+  textureLoader: new THREE.TextureLoader(),
+  particleTex: null,
+  texPath: "/star.png",
 };
 
-function init(canvas: HTMLCanvasElement, canvasRect: DOMRect) {
+async function init(canvas: HTMLCanvasElement, canvasRect: DOMRect) {
   console.log("world init");
 
   _defineCanvas(canvasRect);
 
   _createRenderer(canvas);
   _createCamera();
-  _createMesh();
-
   Utils.setupOrbitControl(world.camera!, canvas);
+
+  await _loadTex();
+
+  _createMesh();
 
   _tick();
 }
@@ -89,15 +99,72 @@ function _defineCanvas(canvasRect: DOMRect) {
   world.canvasRect = canvasRect;
 }
 
+async function _loadTex() {
+  console.log("_loadTex");
+
+  world.textureLoader.manager = world.loadManager;
+
+  world.loadManager.onError = (url) => {
+    console.log("Error", url);
+  };
+  world.loadManager.onProgress = (url, loaded, total) => {
+    console.log("> progress", url, loaded, total);
+  };
+  world.loadManager.onLoad = () => {
+    console.log("∟ loaded.");
+  };
+
+  world.particleTex = await world.textureLoader.loadAsync(world.texPath);
+}
+
 function _createMesh() {
-  const geometry = new THREE.SphereGeometry();
-  const material = new THREE.PointsMaterial();
-  material.size = 0.02;
-  material.sizeAttenuation = true;
+  console.log("_createMesh");
 
-  const mesh = new THREE.Points(geometry, material);
+  // const geometry = new THREE.SphereGeometry();
+  // const material = new THREE.PointsMaterial();
+  // material.size = 0.02;
+  // material.sizeAttenuation = true;
+  // const mesh = new THREE.Points(geometry, material);
 
-  world.scene.add(mesh);
+  const particlesGeometry = new THREE.BufferGeometry();
+  const count = 50000;
+
+  const positions = new Float32Array(count * 3);
+  const colors = new Float32Array(count * 3);
+
+  for (let i = 0; i < count * 3; i++) {
+    positions[i] = (Math.random() - 0.5) * 10;
+    colors[i] = Math.random();
+  }
+
+  particlesGeometry.setAttribute(
+    "position",
+    new THREE.BufferAttribute(positions, 3),
+  );
+  particlesGeometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
+
+  const particlesMaterial = new THREE.PointsMaterial({
+    size: 0.05,
+    sizeAttenuation: true,
+    // color: new THREE.Color("#ff88cc"),
+    // map: world.particleTex,
+    transparent: true,
+    map: world.particleTex,
+    // alphaMap: world.particleTex,
+    // alphaTest: 0.001,
+    // depthTest: false,
+    depthWrite: false,
+    // blending: THREE.AdditiveBlending,
+    vertexColors: true,
+  });
+  const particlesMesh = new THREE.Points(particlesGeometry, particlesMaterial);
+  world.scene.add(particlesMesh);
+
+  // const mesh = new THREE.Mesh(
+  //   new THREE.BoxGeometry(),
+  //   new THREE.MeshBasicMaterial(),
+  // );
+  // world.scene.add(mesh);
 }
 
 export default world;
