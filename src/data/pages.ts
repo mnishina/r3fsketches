30269da583d10957dfee90fd59bfcc6fa62756b8
M Nishina <mitsunorinishina@icloud.com>
2024-11-06 22:36:12 +0900

    Moved pagesData

---
+++ src/data/pages.ts
@@ -0,0 +1,71 @@
+export const pagesData = [
+  {
+    title: "First Scene",
+    url: "/firstScene/",
+  },
+  {
+    title: "Shader Model",
+    url: "/shaderModel/",
+  },
+  {
+    title: "Sphere and Shadow",
+    url: "/sphereAndShadow/",
+  },
+  {
+    title: "Shader Wave",
+    url: "/shaderWave/",
+  },
+  {
+    title: "Shader Smoke",
+    url: "/shaderSmoke/",
+  },
+  {
+    title: "Noise Shader Wave",
+    url: "/noiseShaderWave/",
+  },
+  {
+    title: "Distortion Scroll",
+    url: "/distortionScroll/",
+  },
+  {
+    title: "Wire Sphere",
+    url: "/wireSphere/",
+  },
+  {
+    title: "Texture Earth",
+    url: "/textureEarth/",
+  },
+  {
+    title: "WIP）Shader Earth",
+    url: "/shaderEarth/",
+  },
+  {
+    title: "WIP）Halftone Shade",
+    url: "/halftoneShade/",
+  },
+  {
+    title: "Particles Basic",
+    url: "/particlesBasic/",
+  },
+  {
+    title: "Particles Gallaxy",
+    url: "/particlesGallaxy/",
+  },
+  {
+    title: "Shader Lighting",
+    url: "/shaderLighting/",
+  },
+];
+
+interface PageData {
+  url: string;
+  title: string;
+}
+
+export const getPageTitle = function getPageTitle(
+  data: PageData[],
+  currentPathName: string,
+): string {
+  const page = data.find((d: PageData) => d.url === currentPathName);
+  return page ? page.title : "";
+};
