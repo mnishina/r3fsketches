b893d00375379838a8276594790180b7f42031ee
mnishina <mitsunorinishina@icloud.com>
2024-11-10 23:35:37 +0900

    Added the meshes and lights.

--- src/components/shadowTest/world.ts
+++ src/components/shadowTest/world.ts
@@ -6,6 +6,16 @@ import {
   Mesh,
   BoxGeometry,
   MeshBasicMaterial,
+  SphereGeometry,
+  PlaneGeometry,
+  DoubleSide,
+  DirectionalLight,
+  DirectionalLightHelper,
+  PointLight,
+  PointLightHelper,
+  AmbientLight,
+  MeshStandardMaterial,
+  Clock,
 } from "three";
 
 import Utils from "~/components/Utils/index";
@@ -25,6 +35,7 @@ interface World {
   near: number;
   far: number;
   pixelRatio: number;
+  movingSphereMesh: Mesh | undefined;
   init: (canvas: HTMLCanvasElement) => void;
 }
 
@@ -43,6 +54,7 @@ const world: World = {
   far: 1000,
   renderer: undefined,
   pixelRatio: Math.min(window.devicePixelRatio, 2),
+  movingSphereMesh: undefined,
   init,
 };
 
@@ -63,6 +75,7 @@ function init(canvas: HTMLCanvasElement) {
     world.sizes.width,
     world.sizes.height,
   );
+  _createLight();
   _createMesh();
   _tick();
 
@@ -77,6 +90,7 @@ function _createRenderer(
   world.renderer = new WebGLRenderer({ canvas, antialias: true });
   world.renderer.setSize(canvasWidth, canvasHeight, false);
   world.renderer.setPixelRatio(world.pixelRatio);
+  world.renderer.shadowMap.enabled = true;
 }
 
 function _createCamera(
@@ -90,20 +104,66 @@ function _createCamera(
   aspectRatio = canvasWidth / canvasHeight;
 
   world.camera = new PerspectiveCamera(fov, aspectRatio, near, far);
-  world.camera.position.set(5, 5, 5);
+  world.camera.position.set(-15, 15, 20);
 
   world.scene.add(world.camera);
 }
 
+const clock = new Clock();
+
 function _tick() {
   requestAnimationFrame(_tick);
+
+  const elapsedTime = clock.getElapsedTime();
+
+  world.movingSphereMesh!.position.x = Math.sin(elapsedTime * 2) * 4;
+  world.movingSphereMesh!.position.z = Math.cos(elapsedTime * 2) * 4;
+  world.movingSphereMesh!.position.y = Math.abs(Math.sin(elapsedTime * 8)) + 1;
+
   world.renderer?.render(world.scene, world.camera!);
 }
 
-function _createMesh() {
-  const mesh = new Mesh(new BoxGeometry(), new MeshBasicMaterial());
+function _createLight() {
+  const light = new DirectionalLight(0xffffff, 2);
+  light.position.set(5, 5, 5);
+  light.castShadow = true;
+  const helper = new DirectionalLightHelper(light, 5);
 
-  world.scene.add(mesh);
+  const pointLight = new PointLight(0xffffff, 105);
+  pointLight.position.set(-8, 3, -2);
+  pointLight.castShadow = true;
+  const pointLightHelper = new PointLightHelper(pointLight, 2);
+
+  const ambientLight = new AmbientLight(0xffffff, 0.5);
+
+  world.scene.add(light, helper, ambientLight);
+  world.scene.add(pointLight, pointLightHelper);
+}
+
+function _createMesh() {
+  const sphereMesh = new Mesh(
+    new SphereGeometry(1, 128, 128),
+    new MeshStandardMaterial({ color: "lightGray" }),
+  );
+  sphereMesh.position.set(0, 1, 0);
+  sphereMesh.castShadow = true;
+
+  const movingSphereMesh = new Mesh(
+    new SphereGeometry(1, 128, 128),
+    new MeshStandardMaterial({ color: "lightGray" }),
+  );
+  movingSphereMesh.position.set(0, 0, 0);
+  movingSphereMesh.castShadow = true;
+  world.movingSphereMesh = movingSphereMesh;
+
+  const groundMesh = new Mesh(
+    new PlaneGeometry(10, 10),
+    new MeshStandardMaterial({ color: "gray", side: DoubleSide }),
+  );
+  groundMesh.rotation.set(-Math.PI / 2, 0, 0);
+  groundMesh.receiveShadow = true;
+
+  world.scene.add(sphereMesh, groundMesh, movingSphereMesh);
 }
 
 export default world;
