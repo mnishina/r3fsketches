b38bf17bed62279daa6e3a50ea41075983ce1135
mnishina <mitsunorinishina@icloud.com>
2024-11-13 23:22:42 +0900

    taransform a particle with the fragment shader

--- src/components/particlesGallaxyShader/shaders/fragmentShader.glsl
+++ src/components/particlesGallaxyShader/shaders/fragmentShader.glsl
@@ -1,5 +1,11 @@
 void main() {
-  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
 
-  // #include <colorspace_fragment>
+  // gl_FragColor = vec4(gl_PointCoord, 1.0, 1.0);
+
+
+  float strength = distance(gl_PointCoord, vec2(0.5));
+  strength = 1.0 - step(0.5, strength);
+  gl_FragColor = vec4(vec3(strength), 1.0);
+
+  #include <colorspace_fragment>
 }
