30f48222eae9782b9367b2a7400f204877578ece
mnishina <mitsunorinishina@icloud.com>
2024-11-22 23:08:08 +0900

    :sparkles: Add sky

--- src/components/particlesFireworks/shaders/vertexShader.glsl
+++ src/components/particlesFireworks/shaders/vertexShader.glsl
@@ -4,6 +4,7 @@ uniform float uProgress;
 
 attribute float aSize;
 attribute float aTimeMultiplier;
+
 float remap(float value, float originMin, float originMax, float destinationMin, float destinationMax)
 {
   return destinationMin + (value - originMin) * (destinationMax - destinationMin) / (originMax - originMin);
