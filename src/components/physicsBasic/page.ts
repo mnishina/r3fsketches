834e41cea4d0b146565029324b4da61a5b2405a0
mnishina <mitsunorinishina@icloud.com>
2024-11-27 23:12:52 +0900

    :zap: NaiveBroadphase → SAPBroadphase

--- src/components/physicsBasic/page.ts
+++ src/components/physicsBasic/page.ts
@@ -136,6 +136,7 @@ function _tick(
 function _physicsWorld() {
   //world
   const world = new CANNON.World();
+  world.broadphase = new CANNON.SAPBroadphase(world);
   world.gravity.set(0, -9.82, 0); //地球の重力
 
   //materials
