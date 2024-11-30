35d0ee892e120e0a1c287b86a6c6ce7ee7fbbd09
mnishina <mitsunorinishina@icloud.com>
2024-11-30 21:44:45 +0900

    feat: Create base template

--- src/components/motionHoverEffect/page.ts
+++ src/components/motionHoverEffect/page.ts
@@ -1,13 +1,79 @@
+import * as THREE from "three";
+
 interface Page {
+  numbers: {
+    canvasWidth: number | undefined;
+    canvasHeight: number | undefined;
+    aspectRatio: number | undefined;
+    pixelRatio: number | undefined;
+    camera: {
+      fov: number | undefined;
+      near: number;
+      far: number;
+    };
+  };
+  scene: THREE.Scene;
   init: (canvas: HTMLCanvasElement) => void;
 }
 
 const page: Page = {
+  numbers: {
+    canvasWidth: undefined,
+    canvasHeight: undefined,
+    aspectRatio: undefined,
+    pixelRatio: Math.min(window.devicePixelRatio, 2),
+    camera: {
+      fov: 75,
+      near: 0.1,
+      far: 1000,
+    },
+  },
+  scene: new THREE.Scene(),
   init,
 };
 
 function init(canvas: HTMLCanvasElement) {
   console.log("page init");
+
+  const canvasRect = canvas.getBoundingClientRect();
+  const { width, height } = canvasRect;
+  page.numbers.canvasWidth = width;
+  page.numbers.canvasHeight = height;
+  page.numbers.aspectRatio = width / height;
+
+  const camera = new THREE.PerspectiveCamera(
+    page.numbers.camera.fov,
+    page.numbers.aspectRatio,
+    page.numbers.camera.near,
+    page.numbers.camera.far,
+  );
+  camera.position.set(0, 0, 5);
+
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(page.numbers.canvasWidth, page.numbers.canvasHeight);
+
+  _createMesh();
+
+  _tick(renderer, camera);
+}
+
+function _createMesh() {
+  const mesh = new THREE.Mesh(
+    new THREE.PlaneGeometry(),
+    new THREE.MeshBasicMaterial({ wireframe: true }),
+  );
+  page.scene.add(mesh);
+}
+
+function _tick(renderer: THREE.WebGLRenderer, camera: THREE.PerspectiveCamera) {
+  requestAnimationFrame(() => {
+    _tick(renderer, camera);
+  });
+
+  renderer.render(page.scene, camera);
 }
 
 export default page;
