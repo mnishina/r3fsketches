4f0a0196062ae3aa7628de00673ed5b90185c120
mnishina <mitsunorinishina@icloud.com>
2024-12-10 21:29:06 +0900

    refactor: Cut out the type section.

--- src/components/windImage/page.ts
+++ src/components/windImage/page.ts
@@ -1,37 +1,9 @@
 import * as THREE from "three";
 
+import type { Page } from "./types";
+
 import vertexShader from "./shaders/vertexShader.glsl";
 import fragmentShader from "./shaders/fragmentShader.glsl";
-
-interface ImageInfo {
-  imageTexture: THREE.Texture | undefined;
-  noiseTexture: THREE.Texture | undefined;
-  src: string | undefined;
-  width: number | undefined;
-  height: number | undefined;
-}
-
-interface Page {
-  numbers: {
-    canvasWidth: number | undefined;
-    canvasHeight: number | undefined;
-    devicePixelRatio: number;
-    camera: {
-      fov: number;
-      aspectRatio: number | undefined;
-      near: number;
-      far: number;
-    };
-  };
-  uniforms: {
-    uTexture: { value: THREE.Texture };
-  };
-  imageInfo: ImageInfo;
-  imageInformations: ImageInfo[];
-  noiseAssets: [string, string];
-  scene: THREE.Scene;
-  init: (canvas: HTMLCanvasElement) => void;
-}
 
 const page: Page = {
   numbers: {
